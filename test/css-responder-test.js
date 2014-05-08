1103cd1baec54ccb021cb68f0f2c87e6d328c63f
Shane Tomlinson <stomlinson@mozilla.com>
2014-05-08 12:48:58 +0100

    refactor: Eliminate middleware global variables. All access is through instance variables.
    * The `setup` function returns a middleware instance where all functions and variables can be accessed.
    * This change is to facilitate testing and setting up multiple instances of connect-fonts.

--- test/css-responder-test.js
+++ test/css-responder-test.js
@@ -2,29 +2,30 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var path            = require('path'),
-    nodeunit        = require('nodeunit'),
-    ReqMock         = require('./mocks/req-mock'),
-    ResMock         = require('./mocks/res-mock'),
-    font_config     = require('./sample-config/fonts'),
-    css_responder   = require('../lib/css-responder');
+const nodeunit        = require('nodeunit');
+const ReqMock         = require('./mocks/req-mock');
+const ResMock         = require('./mocks/res-mock');
+const font_config     = require('./sample-config/fonts');
+const css_responder   = require('../lib/css-responder');
 
 // Set a 180 day cache.
 const MAX_AGE = 1000 * 60 * 60 * 24 * 180;
 
+var middleware;
+
 exports.css_responder = nodeunit.testCase({
   setUp: function (cb) {
-    css_responder.reset();
-    css_responder.setup({
+    middleware = css_responder.setup({
       fonts: font_config,
       locale_to_url_keys: {},
       maxage: MAX_AGE,
       compress: true
-    }, cb);
+    });
+    cb();
   },
 
   'generate_css generates CSS': function(test) {
-    css_responder.generate_css("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20130125 Firefox/21.0", "en", ["opensans-regular"], function(err, cssObj) {
+    middleware.generate_css("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20130125 Firefox/21.0", "en", ["opensans-regular"], function(err, cssObj) {
       test.equal(err, null);
       test.ok(cssObj.css.length);
       test.done();
@@ -32,7 +33,7 @@ exports.css_responder = nodeunit.testCase({
   },
 
   'get_css gets CSS': function(test) {
-    css_responder.get_css("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20130125 Firefox/21.0", "en", ["opensans-regular"], function(err, cssObj) {
+    middleware.get_css("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20130125 Firefox/21.0", "en", ["opensans-regular"], function(err, cssObj) {
       test.equal(err, null);
       test.ok(cssObj.css.length);
       test.done();
@@ -40,14 +41,14 @@ exports.css_responder = nodeunit.testCase({
   },
 
   'get_css gets CSS for locale specified in locale-to-subdirs': function(test) {
-    css_responder.get_css("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20130125 Firefox/21.0", "af", ["opensans-regular"], function(err, cssObj) {
+    middleware.get_css("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20130125 Firefox/21.0", "af", ["opensans-regular"], function(err, cssObj) {
       test.equal(err, null);
       test.ok(cssObj.css.indexOf("/fonts/en/opensans-regular.woff") > -1);
       test.done();
     });
   },
 
-  'font_css_responder responds to font.css requests that specify a locale': function(test) {
+  'middleware responds to font.css requests that specify a locale': function(test) {
     var req = new ReqMock({
       url: '/en/opensans-regular/fonts.css',
       method: 'GET',
@@ -62,14 +63,14 @@ exports.css_responder = nodeunit.testCase({
       }
     });
 
-    css_responder.font_css_responder(req, res, function() {
+    middleware(req, res, function() {
       // this should not be called.
       test.ok(false);
       test.done();
     });
   },
 
-  'font_css_responder responds to font.css requests that do not specify a locale- default locale used': function(test) {
+  'middleware responds to font.css requests that do not specify a locale- default locale used': function(test) {
     var req = new ReqMock({
       url: '/opensans-regular/fonts.css',
       method: 'GET',
@@ -85,23 +86,15 @@ exports.css_responder = nodeunit.testCase({
       }
     });
 
-    css_responder.font_css_responder(req, res, function() {
+    middleware(req, res, function() {
       // this should not be called.
       test.ok(false);
       test.done();
     });
   },
 
-  'registerFont throws error before setup has been called': function(test) {
-    css_responder.reset();
-    css_responder.registerFont("font_name", font_config, function(err) {
-      test.ok(err);
-      test.done();
-    });
-  },
-
   'registerFont registers a font after setup has been called': function(test) {
-    css_responder.registerFont("font_name", font_config, function(err) {
+    middleware.registerFont("font_name", font_config, function(err) {
       test.ok(!err);
       test.done();
     });
@@ -111,16 +104,17 @@ exports.css_responder = nodeunit.testCase({
 
 exports.css_responder_with_cdn = nodeunit.testCase({
   setUp: function (cb) {
-    css_responder.reset();
-    css_responder.setup({
+    middleware = css_responder.setup({
       fonts: font_config,
       host: 'https://cdn.testdomain.com',
       locale_to_url_keys: {}
-    }, cb);
+    });
+
+    cb();
   },
 
   'generate_css generates CSS': function(test) {
-    css_responder.generate_css("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20130125 Firefox/21.0", "en", ["opensans-regular"], function(err, cssObj) {
+    middleware.generate_css("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20130125 Firefox/21.0", "en", ["opensans-regular"], function(err, cssObj) {
       test.equal(err, null);
       test.ok(cssObj.css.indexOf('https://cdn.testdomain.com/fonts/en/opensans-regular.woff') > -1);
       test.done();
